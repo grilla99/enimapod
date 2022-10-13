cb1f81820eb214cd3df72ea4f5e1a865429e747d
aaron.grill <aaron.grill@gft.com>
2022-10-13 16:18:31 +0100

    Some Java API code on this branch and containers

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -82,8 +82,22 @@ resource "aws_security_group" "api_task" {
   }
 
   ingress {
+    protocol = "tcp"
+    from_port = 8080
+    to_port = 8080
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
     from_port = 443
     to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -99,15 +113,17 @@ resource "aws_security_group" "api_task" {
 resource "aws_launch_configuration" "ecs_launch_config" {
   image_id = "ami-0fd6a5614931e9e58"
   iam_instance_profile = aws_iam_instance_profile.ecs_agent.name
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   user_data = <<EOF
-#!/bin/bash
+#!/bin/bash 
 # The cluster this agent should check into.
 echo 'ECS_CLUSTER=my-cluster' >> /etc/ecs/ecs.config
 # Disable privileged containers.
 echo 'ECS_DISABLE_PRIVILEGED=true' >> /etc/ecs/ecs.config
 EOF
   associate_public_ip_address = true
+
+  key_name = "MSKKeyPair"
 
   security_groups = [aws_security_group.api_task.id]
 }
@@ -117,14 +133,14 @@ resource "aws_autoscaling_group" "ecs_cluster" {
   vpc_zone_identifier = [aws_subnet.public.id]
   launch_configuration = aws_launch_configuration.ecs_launch_config.name
 
-  desired_capacity = 2
+  desired_capacity = 1
   min_size = 1
   max_size = 3
   health_check_grace_period = 300
   health_check_type = "EC2"
 }
 
-resource "aws_ecr_repository" "this" {
+resource "aws_ecr_repository" "api" {
   name = "enimapod-app-ecr-repository"
 
   tags = {
@@ -132,23 +148,49 @@ resource "aws_ecr_repository" "this" {
   }
 }
 
+resource "aws_ecr_repository" "web" {
+  name = "enimapod-web-server-repository"
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+# Note to Aidan:
+# API Task Works and Runs. 
+# To have both tasks running need an instance with 1024MB (t2.micro and larger??) - Double check this
+# The configuration below is correct for the server but something to do with the Dockerfile / Nginx needs tweaking
+
 resource "aws_ecs_task_definition" "task_definition" {
   family = "service"
   container_definitions = jsonencode([
-    {
-      name = "service"
-      image = "342715877717.dkr.ecr.eu-west-2.amazonaws.com/enimapod-app-ecr-repository:latest"
-      cpu = 2
-      memory = 512
-      essential = true
-      portMappings = [
-        {
-          containerPort = 8081
-          hostPort = 8081
-        }
-      ]
-    }
+   {
+     name = "api"
+     image = "342715877717.dkr.ecr.eu-west-2.amazonaws.com/enimapod-app-ecr-repository:latest"
+     memory = 512
+     essential = true
+     portMappings = [
+       {
+         containerPort = 8081
+         hostPort = 8081
+       }
+     ]
+   }
+  #  ,
+    # {
+    #   name = "server"
+    #   image = "342715877717.dkr.ecr.eu-west-2.amazonaws.com/enimapod-web-server-repository:latest"
+    #   memory = 512
+    #   essential = true
+    #   portMappings = [
+    #     {
+    #       containerPort = 8080
+    #       hostPort = 8080
+    #     }
+    #   ]
+    # }
   ])
+
   volume {
     name = "service-storage"
     host_path = "/ecs/service-storage"
